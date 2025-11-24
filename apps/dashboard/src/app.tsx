1c007fcbb30f4ab171435edc17a924a2b552d8fc
ahargunyllib <billy.bpm03@gmail.com>
2025-11-24 14:20:38 +0700

    feat: Refactor app structure by adding Layout component and removing unused base-button

--- apps/dashboard/src/app.tsx
+++ apps/dashboard/src/app.tsx
@@ -1,20 +1,16 @@
-import { useState } from "react";
+import Layout from "./shared/components/layout";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="isolate">
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((prev) => prev + 1)} type="button">
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
+    <Layout>
+      <Example />
+    </Layout>
+  );
+}
+function Example() {
+  return (
+    <div className="container mx-auto flex flex-col gap-2 py-20">
+      <h1 className="font-bold text-3xl">Hello, Dashboard!</h1>
     </div>
   );
 }
