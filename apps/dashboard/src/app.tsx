8c4dcf2ff00c7bee1fb75e41b20c86156611a6e1
ahargunyllib <billy.bpm03@gmail.com>
2025-11-24 13:32:25 +0700

    feat: Enhance dashboard styles and structure with global CSS variables and layout adjustments

--- apps/dashboard/src/app.tsx
+++ apps/dashboard/src/app.tsx
@@ -4,7 +4,7 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="isolate">
       <h1>Vite + React</h1>
       <div>
         <button onClick={() => setCount((prev) => prev + 1)} type="button">
@@ -15,6 +15,6 @@ export default function App() {
         </p>
       </div>
       <p>Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
