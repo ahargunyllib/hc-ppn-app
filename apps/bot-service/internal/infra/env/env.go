84cdbe8feaa88a6558fcce28b9ba3a2b218fca84
ahargunyllib <billy.bpm03@gmail.com>
2025-11-25 23:22:09 +0700

    feat: Add Google API key configuration to environment settings

--- apps/bot-service/internal/infra/env/env.go
+++ apps/bot-service/internal/infra/env/env.go
@@ -18,18 +18,7 @@ type Env struct {
 	DBName             string        `mapstructure:"DB_NAME"`
 	JwtSecretKey       string        `mapstructure:"JWT_SECRET_KEY"`
 	JwtExpTime         time.Duration `mapstructure:"JWT_EXP_TIME"`
-	RedisAddress       string        `mapstructure:"REDIS_ADDR"`
-	RedisPassword      string        `mapstructure:"REDIS_PASSWORD"`
-	GomailHost         string        `mapstructure:"GOMAIL_HOST"`
-	GomailSenderEmail  string        `mapstructure:"GOMAIL_SENDER_EMAIL"`
-	GomailSenderName   string        `mapstructure:"GOMAIL_SENDER_NAME"`
-	GomailPort         int           `mapstructure:"GOMAIL_PORT"`
-	GomailUsername     string        `mapstructure:"GOMAIL_USERNAME"`
-	GomailPassword     string        `mapstructure:"GOMAIL_PASSWORD"`
-	AWSAccessKeyID     string        `mapstructure:"AWS_ACCESS_KEY_ID"`
-	AWSSecretAccessKey string        `mapstructure:"AWS_SECRET_ACCESS_KEY"`
-	R2AccountID        string        `mapstructure:"R2_ACCOUNT_ID"`
-	OpenAIAPIKey       string        `mapstructure:"OPENAI_API_KEY"`
+	GoogleAPIKey       string        `mapstructure:"GOOGLE_API_KEY"`
 	BotEnabled         bool          `mapstructure:"BOT_ENABLED"`
 	BotDBPath          string        `mapstructure:"BOT_DB_PATH"`
 }
